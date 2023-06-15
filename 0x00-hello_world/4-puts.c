48e61bc7c9e71f8f203946112b31ada78adb8f02
SammerAli <smf550@hotmail.com>
2023-06-15 15:49:46 -0700

    puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main -print a sentence
- *
- * Return: Always 0 (Success)
- */
+  * main - Prints a text
+  *
+  * Return: Always 0 (Success)
+  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
