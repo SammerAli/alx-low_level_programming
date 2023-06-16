e7601197613cc429cf10ce32ba8d6639741f457d
sammerali <smf550@hotmail.com>
2023-06-17 00:39:41 +0300

    pose or not

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,25 +1,30 @@
 #include <stdlib.h>
 #include <time.h>
-/* main - to check positive or negative */
+#include <studio.h>
 
-/* starting the ain function */
+/**
+ * main - prinf id n is positive , negative or zero
+ *
+ * Return: always (0)
+ */
 int main(void)
 {
-		int n;
+	int n;
 
-			srand(time(0));
-				n = rand() - RAND_MAX / 2;
-					if(n > 0)
-					{
-						printf("%d is positive\n", n);
-					}
-					else if(n == 0)
-					{
-						printf("%d is zero\n", n);
-					}
-					else
-					{
-						printf("%d is negative\n", n);
-					}
-					return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		print("%d is negative\n", n);
+	}
+	return (0);
 }
