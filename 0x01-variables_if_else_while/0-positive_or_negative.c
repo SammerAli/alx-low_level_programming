1e81b255a00323eb5b2f74207f4e93c08c152ff3
sammerali <smf550@hotmail.com>
2023-06-17 00:31:29 +0300

    pose or not

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,14 +9,17 @@ int main(void)
 
 			srand(time(0));
 				n = rand() - RAND_MAX / 2;
-					if(n > 0) {
-						printf("is positive");
+					if(n > 0)
+					{
+						printf("%d is positive\n", n);
 					}
-					else if(n == 0) {
-						printf("is zero");
+					else if(n == 0)
+					{
+						printf("%d is zero\n", n);
 					}
-					else{
-						printf("is negative")
+					else
+					{
+						printf("%d is negative\n", n);
 					}
 					return (0);
 }
