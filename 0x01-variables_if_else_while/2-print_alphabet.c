0c78e723c37ad6a0678f67b96230b5d7e7f4913c
sammerali <smf550@hotmail.com>
2023-06-17 01:56:37 +0300

    alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - display alphabet
+ *
+ * Return: always (0)
+ */
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	putchar('\n')
+	return (0);
+}
