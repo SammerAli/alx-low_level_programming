e74d46d9442c69ef6d426dfa022b10663b029425
sammerali <smf550@hotmail.com>
2023-06-17 02:15:46 +0300

    alpha

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,13 +10,17 @@ int main(void)
 	char alpha = 'a';
 	char ALPHA = 'A';
 
-	while (alpha <= 'z') and (ALPHA <= 'Z')
+	while (alpha <= 'z')
 	{
 		putchar(alpha);
 		alpha++;
+	}
+	while (ALPHA <= 'Z')
+	{
 		putchar(ALPHA);
 		ALPHA++;
 	}
+
 	putchar('\n');
 	return (0);
 }
