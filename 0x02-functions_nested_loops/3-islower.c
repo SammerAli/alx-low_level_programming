6e19eace7309b4654232579d4e83fd556c02fd19
SammerAli <smf550@hotmail.com>
2023-06-20 23:24:50 +0300

    lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - check lower case
+ *
+ * Return: 1 for lowercase else 0
+ */
+int _islower(int c)
+{
+	if (islower(c))
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0)
+	}
+}
