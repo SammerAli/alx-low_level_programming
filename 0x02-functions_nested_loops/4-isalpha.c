486a578ddc4c2c1992e2c159baf2b466f2a4114a
SammerAli <smf550@hotmail.com>
2023-06-20 23:35:53 +0300

    alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _alpha - check if it is alphabet
+ *@c: char to be checked
+
+ * Return: 1 for alphabet else 0
+ */
+int _isalpha(int c)
+{
+	int isalpha(int c);
+
+	if (isalpha(c))
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0)
+	}
+}
