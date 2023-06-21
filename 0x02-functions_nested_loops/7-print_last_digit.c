53385875bb9e4e19e0b091fce1c3c17c612d376a
sammerali <smf550@hotmail.com>
2023-06-21 13:11:22 +0300

    last digi

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - print the last digit
+ * @n: the int
+ * return: value of the last digit
+ */
+int print_last_digit(int n)
+{
+	int x = n % 10;
+
+	return (x);
+}
