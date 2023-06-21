0d127c91fdedfa64f20b5270a363c68c18389ee7
sammerali <smf550@hotmail.com>
2023-06-21 15:22:09 +0300

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *add - add 2 numbers
+ *@a: to be checked
+ *@b: to be checked 
+ * Return: return sum
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
