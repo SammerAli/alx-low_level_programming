916effd88399463e16df8831477316cb2d5c4c45
sammerali <smf550@hotmail.com>
2023-06-21 13:18:28 +0300

    last digi

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,11 +2,16 @@
 /**
  * print_last_digit - print the last digit
  * @n: the int
- * return: value of the last digit
+ * Return: value of the last digit
  */
 int print_last_digit(int n)
 {
 	int x = n % 10;
 
+	if (x < 0)
+	{
+		x *= -1;
+	}
+	_putchar(x + '0');
 	return (x);
 }
