4cabcf132e1e65384eec4070272520efa0b7806a
sammerali <smf550@hotmail.com>
2023-06-22 17:20:36 +0300

    multi

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *add - multiply 2 numbers
+ *@a: to be checked
+ *@b: to be checked
+ * Return: return multi
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
