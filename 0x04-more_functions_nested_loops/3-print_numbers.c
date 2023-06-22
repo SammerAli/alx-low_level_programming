88f507d56bba33b58256179c931d971358e1d38f
sammerali <smf550@hotmail.com>
2023-06-22 17:39:39 +0300

    number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+}
