1a291a264d30905f5f5ca0afe51c845e7ce8b0c4
sammerali <smf550@hotmail.com>
2023-06-22 18:24:12 +0300

    number

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - print line
+ * @n: number of _
+ */
+
+void print_line(int n)
+{
+	if (n < 0 && n != 0)
+	{
+		int i;
+
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n')
+	}
+	else
+	{
+		_putchar('\n')
+	}
+}
