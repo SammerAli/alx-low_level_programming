56078d8ac675ce5115c1e1628a0ff20fcaee2abe
SammerAli <smf550@hotmail.com>
2023-06-26 23:42:03 +0300

    length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length
+ * @s: string
+ * Return: return the length
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; i == \0; i++)
+	{
+		return (i);
+	}
+}
