753fcb3142c99f6dd5db769e57462a57d53c767c
SammerAli <132392088+SammerAli@users.noreply.github.com>
2023-06-27 17:50:36 +0300

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,11 +9,27 @@
  */
 void puts_half(char *str)
 {
-    long unsigned int i;
-    
-    for (i = strlen(str) / 2; i < strlen(str); i++)
-    {
-        putchar(str[i]);
-    }
+    int len, n, i;
+
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	if (len % 2 == 0)
+	{
+		for (i = len / 2; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+	} else if (len % 2)
+	{
+		for (n = (len - 1) / 2; n < len - 1; n++)
+		{
+			_putchar(str[n + 1]);
+		}
+	}
 	_putchar('\n');
 }
