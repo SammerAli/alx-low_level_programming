2e3c7a146c5017e140552012832d4c830aae3868
SammerAli <smf550@hotmail.com>
2023-07-07 23:04:03 +0300

    lib

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - check lower case
+ *@c: char to be checked
+ * Return: 1 for lowercase else 0
+ */
+int _islower(int c)
+{
+	int islower(int c);
+
+	if (islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
