f392f6a90f97a300791a2a36ec1e24b9ad66e283
sammerali <smf550@hotmail.com>
2023-07-07 23:35:58 +0300

    lib

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,28 +1,33 @@
 #include "main.h"
 
 /**
- * _strncat - concatenates n bytes from a string to another
- * @dest: destination string
- * @src: source string
- * @n: number of bytes of str to concatenate
- * Return: a pointer to the resulting string dest
+ * _strncat - two words
+ * @dest : pointer to char param
+ * @src : pointer to char param
+ * @n : int parameter
+ * Return: *dest
  */
+
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, c;
+	int m;
+	int i;
 
-	i = 0;
-	c = 0;
+	m = 0;
 
-	for (i = 0; dest[i] != '\0'; i++)
+	for (i = 0; i < 1000; i++)
 	{
+		if (dest[i] == '\0')
+		{
+			break;
+		}
+		m++;
 	}
-	for (c = 0; src[c] != '\0' && c < n; c++)
-	{
-		dest[i] = src[c];
-		i++;
-	}
-	dest[i] = '\0';
 
+	for (i = 0; src[i] != '\0' && i < n; i++)
+	{
+		dest[m + i] = src[i];
+	}
+	dest[m + i] = '\0';
 	return (dest);
 }
