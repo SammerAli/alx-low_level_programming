84723359f475da78e6f0e5b8d048098d3bd42220
SammerAli <smf550@hotmail.com>
2023-07-07 13:15:34 -0700

    lib

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <math.h>
+#include <stdlib.h>
+/**
+ * _abs- print absolute
+ *@c: char to be checked
+ *
+ * Return: absolute value
+ */
+int _abs(int c)
+{
+	int x;
+
+	x = abs(c);
+
+	return (x);
+}
